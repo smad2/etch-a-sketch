230961e58c9bf6c5d2fa786499c10d772468f54b
smad2 <salvamadrid303@gmail.com>
2025-10-08 16:13:24 +0200

    pizarra

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const dom = {
-        sketch: document.getElementById('sketch')
+        sketch: document.getElementById('sketch'),
+        borders: document.getElementById('grid-borders-toggle')
+
     }
 
 
-    createGrid(dom, 100);
+    createGrid(dom, 10);
+    createListeners(dom);
 
 });
 
@@ -27,7 +30,10 @@ function createGrid(dom, grid) {
     for (let i = 0; i < totalCells; i++) {
 
         const grid = document.createElement('div');
-        grid.classList.add('grid');
+        grid.classList.add('grid-item');
+        grid.classList.add('grid-item-border');
+
+
         grid.style.width = cellWidth + "px";
         grid.style.height = cellHeight + "px";
 
@@ -36,5 +42,21 @@ function createGrid(dom, grid) {
         dom.sketch.appendChild(grid);
 
     }
+
+}
+
+function createListeners(dom) {
+    dom.borders.checked = "true"
+
+    const toggleBorders = () => {
+        const grids = document.querySelectorAll(".grid-item");
+        grids.forEach((grid) => {
+            grid.classList.toggle('grid-item-border')
+        });
+
+    }
+
+    dom.borders.addEventListener('change', toggleBorders);
+
 
 }
